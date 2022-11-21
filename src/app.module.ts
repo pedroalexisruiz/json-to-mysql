67d5797d3d0a6567b36335b63f81ea5a6e84789e
pedroruizconekta <alexis.ruiz@conekta.com>
2022-11-21 15:19:16 -0500

    feat(rfactor2): Se agregan entidades al app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,13 @@ import { ACC_ENTITIES } from './acc/model';
 import { ReadJsonModule } from './acc/ReadJson.module';
 import { Rfactor2Module } from './rfactor2/rfactor2.module';
 import { Ac1Module } from './ac1/ac1.module';
+import { RF2_ENTITIES } from './rfactor2/entities';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...BASIC_DB_CONFIG,
-      entities: [...ACC_ENTITIES, ...AC1_ENTITIES],
+      entities: [...ACC_ENTITIES, ...AC1_ENTITIES, ...RF2_ENTITIES],
     }),
     // ReadJsonModule,
     // Ac1Module,
--- src/rfactor2/rfactor2.module.ts
+++ src/rfactor2/rfactor2.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BASIC_DB_CONFIG } from 'src/config/db';
+import { DataSource } from 'typeorm';
 import { RaceDataController } from './controllers/read-race-data.controller';
+import { RF2_ENTITIES } from './entities';
 import { RaceReaderService } from './services/ac1session-reader.service';
 
-@Module({ controllers: [RaceDataController], providers: [RaceReaderService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([...RF2_ENTITIES])],
+  controllers: [RaceDataController],
+  providers: [
+    {
+      provide: 'DATA_SOURCE',
+      useFactory: async () => {
+        const dataSource = new DataSource({
+          ...BASIC_DB_CONFIG,
+          entities: [...RF2_ENTITIES],
+        });
+        return dataSource.initialize();
+      },
+    },
+    RaceReaderService,
+  ],
+})
 export class Rfactor2Module {}
